33a3cbedf562b6315e38146d1a2287466a849ef9
João Manoel de Almeida Lima <83477417+JoaoManoelVK@users.noreply.github.com>
2022-06-04 15:01:54 -0300

    Atualização

--- checking.py
+++ checking.py
@@ -5,11 +5,3 @@ class Checking(Account):
     def __init__(self, name, cpf, address, number, overdraft):
         super().__init__(name, cpf, address, number, overdraft)
         
-
-conta = Checking('Joao Manoel',12312312,'Canoa Quebrada',88988850201,False)
-conta.deposit(30)
-conta.withdraw(40)
-conta.set_overdraft(True)
-conta.withdraw(40)
-conta.extract()
-print(conta.get_balance())
--- savings.py
+++ savings.py
@@ -2,6 +2,10 @@ from conta import Account
 
 
 class savings(Account):
-    def __init__(self, name, cpf, address, number, overdraft):
-        super().__init__(name, cpf, address, number, overdraft)
+    def __init__(self, name, cpf, address, number, balance, overdraft):
+        super().__init__(name, cpf, address, number, balance, overdraft)
         taxes = 0.0017
+
+        def taxas(self):
+            self.__balance =  self.__balance + (self.__balance * taxes) 
+
